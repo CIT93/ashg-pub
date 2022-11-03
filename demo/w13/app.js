bcf1d7ec08bbf5e67d398ba52e19d9e5cf790688
AshtonG2 <ashtong9000@gmail.com>
2022-11-03 18:29:27 +0000

    w13 my code

---
+++ demo/w13/app.js
@@ -0,0 +1,10 @@
+const music = () => {
+    obj = {
+      metal: ["Ace of Spades"],
+      pop: ["Let's Dance"],
+      RandB: ["The Icon is Love"],
+      Folk: ["Sweet Baby James"],
+    };
+  };
+
+  
